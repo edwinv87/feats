f2842a986ec2067638032965d5d0c6e57aecc60c
Edwin Vans <vans.edw@gmail.com>
2020-06-29 22:39:33 +1200

    Update feats_filtering.py

--- feats/feats_filtering.py
+++ feats/feats_filtering.py
@@ -1,15 +1,26 @@
 
 import numpy as np
 
-'''
-Find Highly Variable Genes
-
-
-
-
-'''
 
 def LogFilter(sc):
+    """
+    Computes and stores the log-transformed gene expression data stored in 
+    SingleCell object. Here Log2 is performed after adding a pseudocount of 1.
+
+    Parameters
+    ----------
+
+    sc : SingleCell
+        The SingleCell object containing gene expression data.  
+    
+    Returns
+    -------
+
+    sc : SingleCell
+        The SingleCell object containing the log-transfprmed gene expression data.
+
+    """
+
     X = sc.getCounts()
     X = np.log2(X + 1)
     sc.setCounts(X)
@@ -18,6 +29,33 @@ def LogFilter(sc):
 
 
 def HVGFilter(sc, num_genes, name_by = 'gene_names'):
+    """
+    Filters/Selects the Highly Variable Genes in the SingleCell object by computing 
+    the dispersion of each gene. Returns a sliced SingleCell object containing the top 
+    Highly Variable Genes. Stores additional information such as dispersion ('FEATS_dispersion), 
+    mean ('FEATS_mean') and variance ('FEATS_variance') in the genedata assay of SingleCell 
+    object.
+
+    Parameters
+    ----------
+
+    sc : SingleCell
+        The SingleCell object containing gene expression data.  
+
+    num_genes : int
+        The number of Highly Variable Genes to select. 
+    
+    name_by : str
+        The name of the column in SingleCell object that stores the gene names. This 
+        is used to print the top Highly Variable Genes.
+    
+    Returns
+    -------
+
+    sc : SingleCell
+        A sliced SingleCell object containing the top Highly Variable Genes. 
+
+    """
 
     # compute dispersion and store it in sc object (genedata assay)
     X = sc.getCounts()
@@ -51,6 +89,40 @@ def HVGFilter(sc, num_genes, name_by = 'gene_names'):
 
 
 def GeneFilter(sc, min_cells, max_cells, expr_thres = 0):
+    """
+    Filters the lowly and/or highly expressed genes stored in the SingleCell object 
+    based on the expression counts. Returns the sliced SingleCell object with the
+    genes selected through the filtering criteria. This functions skips filtering and
+    warns the user if the filtering criteria filters out all the genes.
+
+    Parameters
+    ----------
+
+    sc : SingleCell
+        The SingleCell object containing gene expression data.  
+
+    min_cells : int
+        The minimum number of cells in which the gene must be expressed with the 
+        expression threshold. The value should be >= 0 and <= n, where n is the 
+        number of cells the the dataset.  
+    
+    max_cells : int
+        The maximum number of cells in which the gene must be expressed with the 
+        expression threshold. The value should be >= 0 and <= n, where n is the 
+        number of cells the the dataset.  
+
+    expr_thres : int, optional
+        The expression threshold. Default 0. The gene is considered not expressed if
+        the expression count is <= this threshold. 
+    
+    Returns
+    -------
+
+    sc : SingleCell
+        A sliced SingleCell object containing the filtered genes. 
+
+    """
+
 
     print("Applying Gene Filter . . .")
     X = sc.getCounts()
@@ -77,7 +149,40 @@ def GeneFilter(sc, min_cells, max_cells, expr_thres = 0):
 
 
 def CellFilter(sc, min_genes, max_genes, expr_thres = 0):
+    """
+    Filters the cells in which the genes are lowly and/or highly expressed. Returns 
+    the sliced SingleCell object with the cells selected through the filtering criteria. 
+    This functions skips filtering and warns the user if the filtering criteria filters 
+    out all the samples/cells.
+
+    Parameters
+    ----------
+
+    sc : SingleCell
+        The SingleCell object containing gene expression data.  
+
+    min_genes : int
+        The minimum number of genes in the cell in which the genes must be expressed 
+        with the expression threshold. The value should be >= 0 and <= d, where d is the 
+        number of genes the the dataset.  
     
+    max_genes : int
+        The maximum number of genes in the cell in which the gene must be expressed 
+        with the expression threshold. The value should be >= 0 and <= d, where d is the 
+        number of genes the the dataset.  
+
+    expr_thres : int, optional
+        The expression threshold. Default 0. The gene is considered not expressed if
+        the expression count is <= this threshold. 
+    
+    Returns
+    -------
+
+    sc : SingleCell
+        A sliced SingleCell object containing the filtered cells. 
+
+    """
+
     print("Applying Cell Filter . . .")
 
     X = sc.getCounts()
